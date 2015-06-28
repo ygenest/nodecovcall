7c7e88a1e9a70d2f9d7725fab6364be79fe5c0e3
ygenest <yvestablet@gmail.com>
2015-06-28 06:04:34 -0400

    added result page

--- quotes.js
+++ quotes.js
@@ -2,33 +2,21 @@
  * Created by info2301 on 2015/06/26.
  */
 var http = require('http');
-var SpahQL = require('spahql');
+var request = require('request');
+var SpahQL= require('SpahQL');
 module.exports = {
-  getOptionQuotes: function(symbol) {
-    var options = {
-      host: 'www.google.com',
-      path: '/finance/option_chain?q='+symbol+'&output=json'
-    };
-    callback = function(response) {
-      var str = '';
+  getOptionQuotes: function(body) {
+      var sjson = JSON.stringify(eval("(" + body + ")"));
+      var quotes = JSON.parse(sjson);
+      var db = SpahQL.db(quotes);
+      var qta = [];
+      for (var x=0;x<db.select("/calls").value().length;++x) {
+          var qtl=[];
+          qtl[0] = db.select("/calls/"+x+"/expiry");
+          qtl[1] = db.select("/calls/"+x+"/s");
+          qta[x]=qtl;
+      }
+      return qta;
 
-      //another chunk of data has been recieved, so append it to `str`
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
-
-      //the whole response has been recieved, so we just print it out here
-      response.on('end', function () {
-        var sjson = JSON.stringify(eval("(" + str + ")"));
-
-        var quotes=JSON.parse(sjson);
-        var db=SpahQL.db(quotes);
-        var expiry=new Date(db.select("/expiry/y"),db.select("/expiry/m"),db.select("/expiry/d"));
-        callback({
-          expiry:expiry})
-      });
-    }
-    http.request(options, callback).end();
-    return "Le quote de "+exp;
   }
 }
