e78707648ae1ba7db0d3db5ce3ecefffd269181f
ygenest <yvestablet@gmail.com>
2015-06-28 07:23:10 -0400

    added modules

--- quotes.js
+++ quotes.js
@@ -3,19 +3,24 @@
  */
 var http = require('http');
 var request = require('request');
-var SpahQL= require('SpahQL');
+//var SpahQL= require('SpahQL');
 module.exports = {
   getOptionQuotes: function(body) {
       var sjson = JSON.stringify(eval("(" + body + ")"));
       var quotes = JSON.parse(sjson);
-      var db = SpahQL.db(quotes);
+
+      //var db = SpahQL.db(quotes);
       var qta = [];
-      for (var x=0;x<db.select("/calls").value().length;++x) {
+      var x=0;
+      //for (var x=0;x<db.select("/calls").value().length;++x) {
           var qtl=[];
-          qtl[0] = db.select("/calls/"+x+"/expiry");
-          qtl[1] = db.select("/calls/"+x+"/s");
+      qtl[0]="juillet 7";
+      qtl[1]="12.50"
+          //qtl[0] = db.select("/calls/"+x+"/expiry");
+          //qtl[1] = db.select("/calls/"+x+"/s");
           qta[x]=qtl;
-      }
+      // }
+
       return qta;
 
   }
